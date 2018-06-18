36ae0ff19aeab885801a2cec48697d82456eca5e
Solti Dénes <denes.solti@oh.gov.hu>
2018-06-18 19:09:30 +0200

    hibas szerviz nev

--- www/exit.js
+++ www/exit.js
@@ -6,6 +6,6 @@ var exec = require('cordova/exec');
 
 module.exports = {
   exit: function() {
-    exec(null, null, 'Exit', 'exit', Array.from(arguments));
+    exec(null, null, 'ExitPlugin', 'exit', Array.from(arguments));
   }
 };
